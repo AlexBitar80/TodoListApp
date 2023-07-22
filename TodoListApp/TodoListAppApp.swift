63d4c2195e2bf4579c07933659b428ce1dcc5fc7
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-07-22 15:59:44 -0300

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by João Alexandre Bitar on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
