b53923cc11bb3194987d4080f16c961a5eba3d51
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-07-31 14:02:40 -0300

    feat: added parameters to the headerView and add its styles to the registration screen

--- TodoListApp/Views/HeaderView.swift
+++ TodoListApp/Views/HeaderView.swift
@@ -8,32 +8,42 @@
 import SwiftUI
 
 struct HeaderView: View {
+    
+    var title: String
+    var subTitle: String
+    var angle: Double
+    var backgroundColor: Color
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 0)
-                .foregroundColor(Color.pink)
-                .rotationEffect(Angle(degrees: 15))
+                .foregroundColor(backgroundColor)
+                .rotationEffect(Angle(degrees: angle))
             
             VStack {
-                Text("To Do List")
+                Text(title)
                     .font(.system(size: 50))
                     .bold()
                     .foregroundColor(Color.white)
                 
-                Text("Get things done")
+                Text(subTitle)
                     .font(.system(size: 30))
                     .foregroundColor(Color.white)
             }
-            .padding(.top, 30)
+            .padding(.top, 80)
         }
         .frame(width: UIScreen.main.bounds.width * 3,
-               height: 300)
-        .offset(y: -100)
+               height: 350)
+        .offset(y: -150)
     }
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        HeaderView(title: "Hello",
+                   subTitle: "World",
+                   angle: 15,
+                   backgroundColor: .blue)
+    
     }
 }
