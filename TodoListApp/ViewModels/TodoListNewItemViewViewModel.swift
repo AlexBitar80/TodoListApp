21d0799b53659fa5557e15442b896f109984eb58
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-08-30 11:44:04 -0300

    feat: created to-do models and saving items in the database

--- TodoListApp/ViewModels/TodoListNewItemViewViewModel.swift
+++ TodoListApp/ViewModels/TodoListNewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class TodoListNewItemViewViewModel: ObservableObject {
     
@@ -19,7 +21,31 @@ class TodoListNewItemViewViewModel: ObservableObject {
     
     // MARK: - Helpers
     
-    func save() {}
+    func save() {
+        guard canSave else { return }
+        
+        // Get current user id
+        
+        guard let uId = Auth.auth().currentUser?.uid else { return }
+        
+        // Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        
+        // Save model
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+    }
     
     var canSave: Bool {
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
